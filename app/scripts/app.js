c700bd795bd8eea6bfaa434e4f0c1fb68b9b4596
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-10 10:27:01 -0800

    Update input fields and text area for form section

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -73,4 +73,14 @@ angular.module('confusionApp', []).controller('MenuController', ['$scope', funct
     $scope.showDetails = !$scope.showDetails;
   };
 }])
+.controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                               agree:false, email:"" };
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
+        }]);
+
 ;

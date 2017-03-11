3acc6b193f73d49d3948c827b362295544f7ccf6
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-11 14:11:11 -0800

    Update assignment week 2
    Task 1
    
    In this task you will construct the form for users to submit their
    comments. You need to complete the following:
    
    Set up the form with the three fields: name, rating and comment.
    Use a JavaScript object in the controller to keep track of the
    information. The rating should be set to 5 by default. Use radio for
    specifying the rating in the form.
    Use two-way data binding between the form fields and the JavaScript
    object.
    Task 2
    
    In this task, you will enable form validation using Angular support for
    form validation. You need to complete the following:
    
    The name and comments are required fields and the user has entered
    appropriate information in both the fields.
    The submit button should be disabled until the user has entered all the
    required fields.
    The user should be alerted by showing the invalid fields in red, and a
    message displayed at the bottom of the field.
    Task 3
    
    In this task you will enable real-time preview of the user's comments
    on the web page. This should be displayed in the same format as the
    regular comments. You need to complete the following tasks:
    
    Show a preview just above the form using the same format as the regular
    comments on the page.
    Display the preview only if the user has entered valid information into
    the form, and the form is not in its pristine state.
    Upon submitting the valid comment, the comment should join the regular
    comments on the page. The date for the comment should be set
    automatically upon submission of the form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -103,4 +103,88 @@ angular.module('confusionApp', []).controller('MenuController', ['$scope', funct
                 }
             };
         }])
+
+                .controller('DishDetailController', ['$scope', function($scope) {
+
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+                               
+                           ]
+                    };
+            
+            $scope.dish = dish;
+            
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            $scope.name = '';
+            $scope.rating = 5;
+            $scope.comment = '';
+            $scope.date = new Date().toISOString();
+
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
+                // "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+              $scope.date = new Date().toISOString();
+                
+                // Step 3: Push your comment into the dish's comment array
+                //$scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push({
+                  author: $scope.name,
+                  rating: $scope.rating,
+                  comment: $scope.comment,
+                  date: $scope.date
+                });
+                
+                //Step 4: reset your form to pristine
+                $scope.name = '';
+                $scope.rating = 5;
+                $scope.comment = '';
+                $scope.commentForm.$setPristine();
+                
+                //Step 5: reset your JavaScript object that holds your comment
+            };
+
+        }])
+       
 ;
